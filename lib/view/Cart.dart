6f9668ff4887def5ae7fd047ec81313ddeb4be0d
heshanka <info@heshanka.com>
2021-01-05 18:16:22 +0300

    Cart screen base design and func. implemented

--- lib/view/Cart.dart
+++ lib/view/Cart.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/controller/CartModel.dart';
+import 'package:food_delivery_app/controller/MainModel.dart';
+import 'package:food_delivery_app/view/CartItem.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class Cart extends StatelessWidget{
-
+class Cart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My Cart"),
-      ),
-      body: Column(),
-    );
+        appBar: AppBar(
+          title: Text("My Cart"),
+        ),
+        body: ScopedModel(
+            model: MainModel.getCartModel(),
+            child: ScopedModelDescendant<CartModel>(
+                builder: (context, child, model) => Stack(
+                      children: [
+                        ListView.builder(
+                            itemCount: model.productMap.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return CartItem(
+                                  model.productMap.keys.elementAt(index));
+                            }),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 50,
+                            color: Color(0xfffdb827),
+                            child: Center(
+                              child: RichText(
+                                text: TextSpan(
+                                  text: 'TOTAL PRICE: ',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.black),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                        text: model
+                                                .getTotalCartPrice()
+                                                .toStringAsFixed(2) +
+                                            " USD",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold)),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ))));
   }
-
 }
---
+++ lib/view/CartItem.dart
@@ -0,0 +1,63 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/model/Product.dart';
+import '../controller/CartModel.dart';
+import '../controller/MainModel.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class CartItem extends StatelessWidget {
+  final Product product;
+
+  CartItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel(
+      model: MainModel.getCartModel(),
+      child: Column(
+        children: [
+          Container(
+            height: 130,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ScopedModelDescendant<CartModel>(
+                  builder: (context, child, model) => Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                          onTap: () {
+                            model.addToCart(product.id, quantity: 1);
+                          },
+                          child: Icon(Icons.add_circle)),
+                      Text(model.getProductQuantity(product.id).toString()),
+                      InkWell(
+                          onTap: () {
+                            model.removeFromCart(product.id, quantity: 1);
+                          },
+                          child: Icon(Icons.remove_circle)),
+                    ],
+                  ),
+                ),
+                CachedNetworkImage(
+                  placeholder: (context, url) =>
+                      CircularProgressIndicator(), //,
+                  imageUrl: product.imgURL,
+                  width: 100,
+                  height: 120,
+                ),
+                Text(
+                  product.price.toString() + " USD",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+          Divider(
+            color: Colors.grey,
+          )
+        ],
+      ),
+    );
+  }
+}
