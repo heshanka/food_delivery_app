cd95526c596b0d90a62df81163247fb653c71594
heshanka <info@heshanka.com>
2021-01-05 18:15:35 +0300

    moved product list to utils as static

---
+++ lib/Utils.dart
@@ -0,0 +1,34 @@
+import 'model/Product.dart';
+
+class Utils {
+  static List<Product> productList = [
+    Product(
+      1,
+      "Lentil Soup",
+      "",
+      12.5,
+      1,
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.delishknowledge.com%2Fwp-content%2Fuploads%2Fredlentilsoup2.jpg&f=1&nofb=1",
+      true,
+    ),
+    Product(
+      2,
+      "Chicken Burger",
+      "Tomato, Worcestershire, and hot sauce gives this burger a tasty kick, topped with some sweet pickle relish, lettuce and cabbage slaw to balance out the spice.",
+      6.50,
+      3,
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fghk.h-cdn.co%2Fassets%2F17%2F33%2F1502998809-burger-king-grilled-chicken-sandwich.jpg&f=1&nofb=1",
+      true,
+    ),
+    Product(
+      3,
+      "Beef Burger",
+      "",
+      9.20,
+      3,
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.wendys.co.id%2Fassets%2Fmedia%2Fgroup_product%2Fbeef_deluxe.png&f=1&nofb=1",
+      true,
+    ),
+    //Product(1, "Lentil Soup", "", 12.5, 1, "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.delishknowledge.com%2Fwp-content%2Fuploads%2Fredlentilsoup2.jpg&f=1&nofb=1", true, false),
+  ];
+}
