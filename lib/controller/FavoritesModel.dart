c2d50dcfd02f44488fbf8d8cf9dfcfd691a88082
heshanka <info@heshanka.com>
2021-01-07 16:01:49 +0300

    Cleaned the favorites controller code: commented,..

--- lib/controller/FavoritesModel.dart
+++ lib/controller/FavoritesModel.dart
@@ -5,18 +5,23 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../Utils.dart';
 
 class FavoritesModel extends Model {
+  //Holds the favorited product cards (displayed widget) in a list
   List<ProductCard> cardList = [];
+
+  //Contains the list of Products that are favorited
   List<Product> favProductList = [];
-  List<int> favIntIdList = [];
+
   bool favProductListLoaded = false;
   bool favCardsLoaded = false;
 
   Future getFavProductCardList() async {
     if (!favCardsLoaded) {
-      await getFavListFromStringIds();
+      //Get the list of favorited Products stored locally first
+      await getFavoritedProducts();
       if (favProductListLoaded && favProductList.isNotEmpty) {
         cardList = [];
         for (Product p in favProductList) {
+          //Add the products as ProductCard widgets to cardList
           cardList.add(ProductCard(p));
         }
         favCardsLoaded = true;
@@ -25,6 +30,7 @@ class FavoritesModel extends Model {
     }
   }
 
+  //Adds the favorited Products to cardList as product cards
   void getLocalFavProductCardList() {
     cardList = [];
     if (favProductList.isNotEmpty) {
@@ -35,19 +41,24 @@ class FavoritesModel extends Model {
     }
   }
 
+  //Make a list of string IDs from the product IDs added to favProductList
   List<String> getStringIDList() {
     List<String> favIdList = [];
     for (Product p in favProductList) {
       if (!favIdList.contains(p.id)) {
+        //to avoid duplicate entries
         favIdList.add(p.id.toString());
       }
     }
     return favIdList;
   }
 
-  Future getFavListFromStringIds() async {
+  /*This method helps retrieve the favorited products in a list, from the local
+    storage with shared preferences*/
+  Future getFavoritedProducts() async {
     if (!favProductListLoaded) {
       SharedPreferences prefs = await SharedPreferences.getInstance();
+      //retreive the list with key 'favList'
       List<String> stringIds = prefs.getStringList('favList');
       for (String pId in stringIds) {
         for (Product product in Utils.productList) {
@@ -61,26 +72,36 @@ class FavoritesModel extends Model {
     }
   }
 
+  /*Called when user clicks on the grey heart icon, saves a list of String Ids from
+  product ids into local storage with shared preferences. **Usually added to a 
+  database, but for the scope of this project, the local storage is used*/
+
   Future addToFavorites(int productId, SharedPreferences prefs) async {
+    //Find the product from the product list with the productId and add it to favProductList
     for (Product p in Utils.productList) {
       if (p.id == productId && !favProductList.contains(p)) {
         favProductList.add(p);
         break;
       }
     }
+    //Save the product ids as a list of strings e.g. ['1','7','5']
     await prefs.setStringList("favList", getStringIDList());
     getLocalFavProductCardList();
     notifyListeners();
   }
 
+  /*Called when the user clicks on the red heart icon; To remove a product from
+  the list of favorites, which is stored as a String list of product IDs, it is
+  required to reset the list with the updated version*/
   Future removeFromFavorites(int productId, SharedPreferences prefs) async {
-    //SharedPreferences prefs = await SharedPreferences.getInstance();
+    //first remove it from favProductList
     for (Product p in Utils.productList) {
       if (p.id == productId) {
         favProductList.remove(p);
         break;
       }
     }
+    //save the updated list with the same shared pref string key
     await prefs.setStringList("favList", getStringIDList());
     getLocalFavProductCardList();
     notifyListeners();
