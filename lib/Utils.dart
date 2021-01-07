d29fbed15ea8ae8311b484cc6f7ef97d36cee98a
heshanka <info@heshanka.com>
2021-01-07 16:02:09 +0300

    Updated Product List in utils

--- lib/Utils.dart
+++ lib/Utils.dart
@@ -1,6 +1,9 @@
 import 'model/Product.dart';
+import 'package:intl/intl.dart';
 
 class Utils {
+  static var numFormat = new NumberFormat("###.00", "en_US");
+
   static List<Product> productList = [
     Product(
       1,
@@ -8,7 +11,7 @@ class Utils {
       "",
       12.5,
       1,
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.delishknowledge.com%2Fwp-content%2Fuploads%2Fredlentilsoup2.jpg&f=1&nofb=1",
+      "https://menu.indolj.com/upload/1584438166-1553264528-12-Lentil-Soup-\$4.99.jpeg-2.png",
       true,
     ),
     Product(
@@ -17,7 +20,7 @@ class Utils {
       "Tomato, Worcestershire, and hot sauce gives this burger a tasty kick, topped with some sweet pickle relish, lettuce and cabbage slaw to balance out the spice.",
       6.50,
       3,
-      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fghk.h-cdn.co%2Fassets%2F17%2F33%2F1502998809-burger-king-grilled-chicken-sandwich.jpg&f=1&nofb=1",
+      "http://ghk.h-cdn.co/assets/17/33/1502998809-burger-king-grilled-chicken-sandwich.jpg",
       true,
     ),
     Product(
@@ -26,9 +29,44 @@ class Utils {
       "",
       9.20,
       3,
-      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.wendys.co.id%2Fassets%2Fmedia%2Fgroup_product%2Fbeef_deluxe.png&f=1&nofb=1",
+      "http://www.wendys.co.id/assets/media/group_product/beef_deluxe.png",
       true,
     ),
-    //Product(1, "Lentil Soup", "", 12.5, 1, "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.delishknowledge.com%2Fwp-content%2Fuploads%2Fredlentilsoup2.jpg&f=1&nofb=1", true, false),
+    Product(
+      4,
+      "Cheese Lovers",
+      "",
+      5.555,
+      2,
+      "https://www.cottageinn.com/app/uploads/2014/07/GarlicCheeseBread_sm1.png",
+      true,
+    ),
+    Product(
+      5,
+      "Fried Rice",
+      "",
+      8.70,
+      4,
+      "https://eatchinwa.com/wp-content/uploads/2019/04/Chinwa-Chinwa%E2%80%99s-Fried-Rice-Chicken.png",
+      true,
+    ),
+    Product(
+      6,
+      "Dumplings",
+      "",
+      10.50,
+      4,
+      "http://www.bibigo.com/img/en/obj_home1_1.png",
+      true,
+    ),
+    Product(
+      7,
+      "Pepperoni Pizza",
+      "",
+      7.90,
+      2,
+      "https://forums.ultra-combo.com/uploads/default/original/2X/8/89674b29f7366334aae4ad532e230bd04f7a07df.png",
+      true,
+    ),
   ];
 }
