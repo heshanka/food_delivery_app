e4374c6844cd2fde95aa659049cbfa7341759066
heshanka <info@heshanka.com>
2021-01-07 22:46:30 +0300

    Updated home screen design with a carousel

--- lib/view/Categories.dart
+++ lib/view/Categories.dart
@@ -4,6 +4,7 @@ import '../model/Category.dart';
 import '../view/CategoryCard.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 import '../Utils.dart';
 
@@ -18,50 +19,99 @@ class Categories extends StatelessWidget {
     return cardlist;
   }
 
+  List<Widget> lw = [
+    Image.asset(
+      "assets/pizzacamp.png",
+    ),
+    Image.asset(
+      "assets/test.png",
+    ),
+    Image.asset(
+      "assets/madefresh.png",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    double scaleFactor = Utils.getScreenWidth(context) / Utils.pWidth;
     return ScopedModel(
       model: model,
       child: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Text(
-              "Welcome to",
-              style: GoogleFonts.alegreya(
-                  fontSize: 32,
-                  color: Colors.black,
-                  fontWeight: FontWeight.normal),
-            ),
-          ),
-        ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(left: 16),
+            padding: EdgeInsets.only(top: 25.0 * scaleFactor, bottom: 16.0 * scaleFactor),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                Text(
+                  "Welcome to Foodiez!",
+                  style: GoogleFonts.meddon(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal),
+                      textScaleFactor: scaleFactor,
+                ),
+                // Container(
+                //   width: 170,
+                //   height: 30,
+                //   decoration: BoxDecoration(borderRadius: BorderRadius.circular(12), color: Color(0xfff6f6f6)),
+                //   child: Center(child:Text("Set delivery address >", 
+                //   style: TextStyle(fontSize: 13, color: Colors.black),),),),
+              ],
+            ),
+          ),
+            Padding(
+              padding: EdgeInsets.only(left: 16 * scaleFactor),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Foodiez!",
-                    style: GoogleFonts.alegreya(
-                        fontSize: 32,
-                        color: Colors.black,
-                        fontWeight: FontWeight.normal),
+                  // Text(
+                  //   "Foodiez!",
+                  //   style: GoogleFonts.alegreya(
+                  //       fontSize: 32,
+                  //       color: Colors.black,
+                  //       fontWeight: FontWeight.normal),
+                  // ),
+                  // Padding(
+                  //   padding: const EdgeInsets.only(top: 16.0),
+                  //   child: Text(
+                  //     "Today's Deals",
+                  //     style:
+                  //         TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  //   )
+                  // ),
+                  Container(
+                    height: 200 * scaleFactor,
+                    child: Swiper(
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          child: lw[index],
+                        );
+                      },
+                      itemCount: lw.length,
+                      pagination: new SwiperPagination(
+                        builder: SwiperPagination.dots,
+                        margin: EdgeInsets.all(5)),
+                      control: new SwiperControl(size: 20),
+                      //containerHeight: 150,
+                    ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 16.0),
+                    padding: EdgeInsets.only(top: 16.0 * scaleFactor, left: 8 * scaleFactor),
                     child: Text(
-                      "Categories",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
+                    "Categories",
+                    style: GoogleFonts.courgette(
+                        fontSize: 22,
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal),
+                        textScaleFactor: scaleFactor,
+                  ),
                   ),
                 ],
               ),
@@ -69,10 +119,11 @@ class Categories extends StatelessWidget {
             Expanded(
               child: GridView.count(
                 primary: false,
-                padding: const EdgeInsets.all(20),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
+                padding: EdgeInsets.all(20 * scaleFactor),
+                crossAxisSpacing: 10 * scaleFactor,
+                mainAxisSpacing: 10 * scaleFactor,
                 crossAxisCount: 2,
+                childAspectRatio: 0.9,
                 children: returnCategoryCardList(),
               ),
             ),
